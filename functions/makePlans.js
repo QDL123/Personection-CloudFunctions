80c23f60bd7ce93cc73f06394590424a6e7d3ac5
qdl <qdl@umich.edu>
2019-07-20 14:39:10 -0500

    first draft through blocking comformance

--- functions/makePlans.js
+++ functions/makePlans.js
@@ -7,55 +7,70 @@ exports.handler = async (snap, context) => {
     const { log } = console;
     log(`version: 2.7`);
 
+    // Data aggregation
+
     let db = admin.firestore();
 
-    const userID = context.params.userID;
-    const startTime = snap.data().startTime;
-    const endTime = snap.data().endTime;
+    const { userID } = context.params;
+    const { startTime } = snap.data();
+    const { endTime } = snap.data();
 
-    const friends = await db.collection('users').doc(userID).collection('friends').listDocuments();
+    const userPath = db.collection('users').doc(userID);
+    const blockedPromise = userPath.collection('blocked').listDocuments();
+    const friendsPromise = await userPath.collection('friends').listDocuments();
 
+
+    const relations = await Promise.all([blockedPromise, friendsPromise]);
+    // const blocked = relations[0];
+    const friends = relations[1];
+
+    // get all friend plan info
+    // friendsPlans is an array of plans objects that passed relation, time, and blocking conformance
     const friendsPlans = await Promise.all(
+        // Relation conformance
         friends.map(friend => {
-            return getFriendPlans(db, friend);
+            const params = { startTime, endTime, db, friend, blocked, userID };
+            return getFriendPlans(params);
         })
     );
 
-    // const friendsPlans = await Promise.all(
-    //     friends.map(friend => {
-    //         return async () => {
-    //             const friendID = friend.id;
-    //             const plansRefs = await db.collection('users').doc(friendID).collection('plans').listDocuments();
-    //             const plans = await Promise.all(
-    //                 plansRefs.map(ref => {
-    //                     return ref.get();
-    //                 })
-    //             );
-    //             return {
-    //                 friendID,
-    //                 plans
-    //             };
-    //         };
-    //     })
-    // );
+    // Consolidate recurrences
+    const consolidated
+}
 
-    friendsPlans.forEach(friend => {
-        friend.plans.forEach(plan => {
-            log(`startTime: ${plan.data().startTime}`);
-        })
+async function getFriendPlans(params) {
+    const friendID = params.friend.id;
+    const plansRefs = await params.db.collection('users').doc(friendID).collection('plans').listDocuments();
+    const plans = plansRefs.map(ref => {
+        // stuff the friend attribute inside the plan data strucuture
+        const plan = ref.get().friendIDs.push(friendID);
     })
-}
 
-async function getFriendPlans(db, friend) {
-    const friendID = friend.id;
-    const plansRefs = await db.collection('users').doc(friendID).collection('plans').listDocuments();
-    const plans = await Promise.all(
-        plansRefs.map(ref => {
-            return ref.get();
+    const timeConformancePlans = plans.filter(plan => {
+        // checking time conformance
+        const oneHour = 7200;
+        if (Math.min(endTime, plan.data().endTime) - Math.max(startTime, plan.data().startTime) > oneHour) {
+            return true;
+        }
+        return false;
+    })
+
+    // filter time conformance results for blocking conformance
+    return await Promise.all(
+        timeConformancePlans.filter(async plan => {
+            plan.members.forEach(member => {
+                // NEEDS SYNTAX CHECK
+                if (params.blocked.includes(member.id)) {
+                    return false;
+                } else {
+                    const memberBlockList = await params.db.collection.user(member.id).collection('blocked').listDocuments();
+                    if (memberBlockList.includes(userID)) {
+                        return false
+                    }
+                }
+            })
+            return true;
         })
     );
-    return {
-        friendID,
-        plans
-    };
 }
+
